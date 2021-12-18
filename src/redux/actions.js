edcf881ba08b2f733c6fcc9d338d1d949bc73b3e
sash <na-okne@mail.ru>
2021-12-18 22:53:20 +0300

    actions

---
+++ src/redux/actions.js
@@ -0,0 +1,16 @@
+
+
+export const all = () => ({ type: 'ALL' });
+export const none = () => ({ type: 'NONE' });
+export const one = () => ({ type: 'ONE ' });
+export const two = () => ({ type: 'TWO' });
+export const three = () => ({ type: 'THREE' });
+export const cheapest = () => ({ type: 'CHEAPEST' });
+export const notCheapest = () => ({ type: 'NOT_CHEAPEST' });
+
+// function addTodo(text) {
+// 	return {
+// 	  type: ADD_TODO,
+// 	  text
+// 	}
+//  }
